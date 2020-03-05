07c15f0ff7654aefc87769d936db7b03b3fc41fb
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-03-05 14:07:47 +0000

    Config Serve and MongoDB

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var logger = require("morgan"),
 cors = require("cors"),
 http = require("http"),
 express = require("express"),
-bodyParser = require("body-parser");
+bodyParser = require("body-parser"),
+mongoose = require( 'mongoose');
 
 var app = express();
 var port = 3000;
@@ -13,4 +14,13 @@ app.use(require('./routes'));
 
 app.listen(port, function(err){
     console.log("Listening on Port: " + port)
+});
+
+mongoose.connect('mongodb://localhost/test');
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
 });
